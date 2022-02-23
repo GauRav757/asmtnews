d074eafb65181a8e2778a9921169e1855af77163
Gaurav Khati <gaurav@gmail.com>
2022-02-23 14:33:11 +0545

    hi

--- category.php
+++ category.php
@@ -9,7 +9,8 @@ session_start();
 $result = mysqli_query($conn,$query);
 $data = mysqli_fetch_assoc($result);
 
-
+$categoryQuery = "SELECT * FROM category";
+$categoryResult = mysqli_query($conn,$categoryQuery);
 
 ?>
 
@@ -25,14 +26,45 @@ $data = mysqli_fetch_assoc($result);
   <div class="row">
   <?php include('include/left-nav.php');?>
       <div class="col-8">
-       <form method="POST" action="#">
+       <form method="POST" action="db/add-category.php">
            <label>Category title</label>
            <div class="input-group">
                <input type="text" class="form-control" name="category">
            </div>
+           <br/>
+           <div class="input-group">
+               <input type="text" placeholder="fa icon class" class="form-control" name="iconclass">
+           </div>
+           <br/>
+           
         <br/>
         <button type="submit" class="btn btn-primary">Save</button>
-</form>
+      </form>
+        <?php include('include/message.php'); ?>
+
+        <div class="row justify-content-md-center"></div>
+        <?php
+          if(mysqli_num_rows($categoryResult)==0){
+            echo "<h3>No Category found</h3>";
+           }else{ ?>
+
+           <table class="table">
+             <thead>
+               <th>Title</th>
+               <th>Action</th>
+           </thead>
+           <tbody>
+             <?php while($row=mysqli_fetch_assoc($categoryResult)) { ?>         
+           <tr>            
+             <td><?php echo $row['title'];?></td>
+             <td>D | E</td>
+           </tr>
+           <?php } ?>
+           <tbody>
+             </table>
+            
+          <?php }
+          ?>
       </div>
     </div>
   </div>
